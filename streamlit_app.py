1db4a1b9c3b447a066520729f3f96242a28238eb
MartinFeldges <120381001+MartinFeldges@users.noreply.github.com>
2022-12-23 16:00:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,10 +18,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
 # Display the table on the page.
-
-
-
 streamlit.dataframe(fruits_to_show)
+
+#User filter
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 # Request
 streamlit.header("Fruityvice Fruit Advice!")
